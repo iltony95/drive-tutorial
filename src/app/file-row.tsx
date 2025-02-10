aeef80f87ebe4e64e6845e6171b051db734f1852
iltony95 <iltony.95@gmail.com>
2025-02-10 23:54:23 +0100

    Connect DB

--- src/app/file-row.tsx
+++ src/app/file-row.tsx
@@ -1,7 +1,7 @@
 import { FileIcon, Folder as LFolder } from "lucide-react";
-import type { Folder, File } from "@/lib/mock-data";
+import type { files, folders } from "~/server/db/schema";
 
-export function FileRow(props: { file: File }) {
+export function FileRow(props: { file: typeof files.$inferSelect }) {
   const { file } = props;
 
   return (
@@ -28,7 +28,7 @@ export function FileRow(props: { file: File }) {
 }
 
 export function FolderRow(props: {
-  folder: Folder;
+  folder: typeof folders.$inferSelect;
   handleFolderClick: () => void;
 }) {
   const { folder, handleFolderClick } = props;
